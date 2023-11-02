929dc14da79b6eaef8281de38fc84a30da6c3067
nigel chua <104724840+NNIIGGEE@users.noreply.github.com>
2023-11-02 16:09:50 +0800

    new

--- script.js
+++ script.js
@@ -1,51 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
     const wheel = document.querySelector(".wheel");
     const spinButton = document.getElementById("spin-button");
-    const sections = document.querySelectorAll(".wheel-section");
-    let spinning = false;
-    let userChoices = []; // Store user's custom choices
+    const sections = []; // Store user choices
 
-    // Function to retrieve user's custom choices from the URL
+    // Function to retrieve user choices from the URL
     function getUserChoices() {
         const urlParams = new URLSearchParams(window.location.search);
         for (const param of urlParams) {
             if (param[0] === "choice") {
-                userChoices.push(param[1]);
+                sections.push(param[1]);
             }
         }
     }
 
-    // Function to spin the wheel
-    function spinWheel() {
-        if (spinning || userChoices.length === 0) return;
-
-        spinning = true;
-        const rotationTime = 4000; // Time in milliseconds
-        const totalRotation = 10 * 360; // 10 full rotations
-        const randomRotation = Math.floor(Math.random() * 360);
-        const totalDegrees = totalRotation + randomRotation;
-        const degreesPerStep = 1;
-        let currentRotation = 0;
-
-        spinButton.disabled = true;
-
-        const spinInterval = setInterval(function () {
-            currentRotation += degreesPerStep;
-            wheel.style.transform = `rotate(${currentRotation}deg)`;
-
-            if (currentRotation >= totalDegrees) {
-                clearInterval(spinInterval);
-
-                const selectedSection = Math.floor((360 - (currentRotation % 360)) / (360 / userChoices.length));
-                alert(`You've won: ${userChoices[selectedSection]}`);
-                spinning = false;
-                spinButton.disabled = false;
-            }
-        }, rotationTime / totalDegrees);
+    // Function to create sections on the wheel based on user choices
+    function createSections() {
+        for (const choice of sections) {
+            const section = document.createElement("div");
+            section.className = "wheel-section";
+            section.textContent = choice;
+            wheel.appendChild(section);
+        }
     }
 
-    // Initialize user's custom choices
+    // Initialize user's custom choices and create sections
     getUserChoices();
+    createSections();
+
+    // Function to spin the wheel
+    function spinWheel() {
+        const totalDegrees = sections.length * (360 / sections.length);
+        // Modify the spinning wheel logic to use user's choices
+        // Use sections for the spinning wheel
+        // Your spinning wheel logic here
+    }
 
     // Event listener for the spin button
     spinButton.addEventListener("click", spinWheel);
