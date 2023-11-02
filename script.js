ed97db669052130617d543311aefcce28e5490f6
nigel chua <104724840+NNIIGGEE@users.noreply.github.com>
2023-11-02 16:26:16 +0800

    last push

--- script.js
+++ script.js
@@ -16,10 +16,14 @@ document.addEventListener("DOMContentLoaded", function () {
     
     // Function to dynamically create sections based on user choices
     function createSections(choices) {
-        for (const choice of choices) {
+        const totalChoices = choices.length;
+        const degreesPerSection = 360 / totalChoices;
+        
+        for (let i = 0; i < totalChoices; i++) {
             const section = document.createElement("div");
             section.className = "wheel-section";
-            section.textContent = choice;
+            section.textContent = choices[i];
+            section.style.transform = `rotate(${i * degreesPerSection}deg)`;
             wheel.appendChild(section);
         }
     }
@@ -27,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Function to spin the wheel
     function spinWheel() {
         // Your spinning wheel logic here
+        // You can use CSS animations or JavaScript for the spinning effect
     }
     
     // Initialize user's custom choices and create sections
