8849c3439c267a2c1b5828305ff7949cb66ffe2b
nigel chua <104724840+NNIIGGEE@users.noreply.github.com>
2023-11-02 16:01:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const wheel = document.querySelector(".wheel");
-    const sections = document.querySelectorAll(".wheel-section");
-    const spinButton = document.getElementById("spin-button");
-
-    let spinning = false;
-
-    function spinWheel() {
-        if (spinning) return;
-
-        spinning = true;
-        const rotationTime = 4000; // Time in milliseconds
-        const totalRotation = 10 * 360; // 10 full rotations
-        const randomRotation = Math.floor(Math.random() * 360);
-        const totalDegrees = totalRotation + randomRotation;
-        const degreesPerStep = 1;
-        let currentRotation = 0;
-
-        spinButton.disabled = true;
-
-        const spinInterval = setInterval(function () {
-            currentRotation += degreesPerStep;
-            wheel.style.transform = `rotate(${currentRotation}deg)`;
-
-            if (currentRotation >= totalDegrees) {
-                clearInterval(spinInterval);
-
-                const selectedSection = Math.floor((360 - (currentRotation % 360)) / 72);
-                alert(`You've won: ${sections[selectedSection].textContent}`);
-                spinning = false;
-                spinButton.disabled = false;
-            }
-        }, rotationTime / totalDegrees);
+function spinWheel() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const choices = [];
+    for (const param of urlParams) {
+        if (param[0] === "choice") {
+            choices.push(param[1]);
+        }
     }
 
-    spinButton.addEventListener("click", spinWheel);
-});
+    // Use the choices array for spinning wheel logic
+    // Modify the spinning wheel logic to use the choices array for options
+    if (choices.length > 0) {
+        alert(`Spinning the wheel with choices: ${choices.join(', ')}`);
+        // Your spinning wheel logic here
+    } else {
+        alert("No choices provided.");
+    }
+}
